9aa6ce3c17268df727ce933cfe0c8a5c1f099481
Thanh <tatthanh.dev@gmail.com>
2024-03-27 16:35:46 +0700

    migrate Posts CRUD using dynamodb

--- lib/news_2_post/crud/post.ex
+++ lib/news_2_post/crud/post.ex
@@ -4,18 +4,18 @@ defmodule News2Post.CRUD.Post do
 
   schema "posts" do
     field :title, :string
-    field :preamble, :string
+    field :description, :string
     field :sections, :string
     field :status, :string  # raw || reviewing || approved || published
     field :url, :string
 
-    timestamps(type: :utc_datetime)
+    timestamps(type: :utc_datetime, inserted_at: :created_at, updated_at: :updated_at)
   end
 
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:title, :preamble, :sections, :status, :url])
-    |> validate_required([:title, :preamble, :sections])
+    |> cast(attrs, [:title, :description, :sections, :status, :url])
+    |> validate_required([:title, :description, :sections])
   end
 end
--- lib/news_2_post_web/controllers/post_controller.ex
+++ lib/news_2_post_web/controllers/post_controller.ex
@@ -3,10 +3,17 @@ defmodule News2PostWeb.PostController do
 
   alias News2Post.CRUD
   alias News2Post.CRUD.Post
+  alias ExAws.Dynamo
+
 
   def index(conn, _params) do
+    table_name = "news2post_dev_Posts_v2"
     status = Map.get(conn.params, "status", "all");
-    posts = CRUD.list_posts(status)
+#    posts = CRUD.list_posts(status)
+
+    posts = all_posts_v2(table_name)
+#    IO.inspect(posts)
+
     render(conn, :index, posts: posts, status: status)
   end
 
@@ -28,70 +35,137 @@ defmodule News2PostWeb.PostController do
   end
 
   def show(conn, %{"id" => id}) do
-    post = CRUD.get_post!(id)
-    IO.inspect(post.sections)
-    IO.puts("post.sections: #{inspect(post.sections, pretty: true)}")
+
+    table_name = "news2post_dev_Posts_v2"
+#    result = Dynamo.get_item(table_name, %{id: "40b3c688-eb40-11ee-ac52-6e7281ad5aa1", created_at: "2024-03-26 07:12:49.102209Z"})
+#             |> ExAws.request!
+#             |> Dynamo.decode_item(as: Post)
+#    IO.puts("result: #{inspect(result, pretty: true)}")
+
+
+    post = get_post_by_id(id)
+#    IO.puts("post: #{inspect(post, pretty: true)}")
+
+#    post = CRUD.get_post!(id)
+#    IO.inspect(post.sections)
+#    IO.puts("post.sections: #{inspect(post.sections, pretty: true)}")
 
     sections = JSON.decode!(post.sections)
-    IO.puts("sections: #{inspect(sections, pretty: true)}")
+#    IO.puts("sections: #{inspect(sections, pretty: true)}")
 
     render(conn, :show, post: post, sections: sections)
   end
 
   def edit(conn, %{"id" => id}) do
-    post = CRUD.get_post!(id)
+#    post = CRUD.get_post!(id)
+    post = get_post_by_id(id)
     changeset = CRUD.change_post(post)
     sections = JSON.decode!(post.sections)
-    IO.puts("sections: #{inspect(sections, pretty: true)}")
+#    IO.puts("sections: #{inspect(sections, pretty: true)}")
 
     render(conn, :edit, post: post, changeset: changeset, sections: sections)
   end
 
   def update(conn, %{"id" => id, "post" => post_params}) do
-    post = CRUD.get_post!(id)
+    post = get_post_by_id(id)
 
     sections = Map.values(post_params["sections"])
     sections = JSON.encode!(sections)
 
     updated_data = %{post_params | "sections" => sections}
-    IO.puts("updated_data: #{inspect(updated_data, pretty: true)}")
-    IO.inspect("is_map updated_data: #{is_map(updated_data)}")
+    table_name = "news2post_dev_Posts_v2"
+    result = Dynamo.update_item(
+               table_name,
+               %{id: post.id, created_at: post.created_at},
+               %{
+                 update_expression: "SET #title = :title, #description = :description, #sections = :sections",
+                 expression_attribute_names: %{"#title" => "title", "#description" => "description", "#sections" => "sections"},
+                 expression_attribute_values: %{:title => updated_data["title"], :description => updated_data["description"], :sections => updated_data["sections"]},
+                 return_values: :updated_new
+               }
+             )
+             |> ExAws.request!
+    IO.puts("update_item result: #{inspect(result, pretty: true)}")
 
-    case CRUD.update_post(post, updated_data) do
-      {:ok, post} ->
-        conn
-        |> put_flash(:info, "Post updated successfully.")
-        |> redirect(to: ~p"/posts/#{post}")
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, :edit, post: post, changeset: changeset)
-    end
+    conn
+    |> put_flash(:info, "Post updated successfully.")
+    |> redirect(to: ~p"/posts/#{post}")
   end
 
   def approve(conn, params) do
-    status = %{
-      "status" => "approved"
-    }
-    IO.puts("status: #{inspect(status, pretty: true)}")
+#    status = %{
+#      "status" => "approved"
+#    }
+#    IO.puts("status: #{inspect(status, pretty: true)}")
 
-    post = CRUD.get_post!(params["id"])
-    case CRUD.update_post(post, status) do
-      {:ok, _post} ->
-        conn
-        |> put_flash(:info, "Post Approved successfully.")
-        |> redirect(to: ~p"/posts")
+#    post = CRUD.get_post!(params["id"])
+    post = get_post_by_id(params["id"])
+#    IO.puts("..... post: #{inspect(post, pretty: true)}")
+    table_name = "news2post_dev_Posts_v2"
+    Dynamo.update_item(
+      table_name,
+      %{id: post.id, created_at: post.created_at},
+      %{
+        update_expression: "SET #status = :status",
+        expression_attribute_names: %{"#status" => "status"},
+        expression_attribute_values: %{:status => "approved"},
+        return_values: :updated_new
+      }
+    )
+    |> ExAws.request!
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, :edit, post: post, changeset: changeset)
-    end
+    conn
+    |> put_flash(:info, "Post Approved successfully.")
+    |> redirect(to: ~p"/posts")
+
+#    |> ExAws.request!
+#    case CRUD.update_post(post, status) do
+#      {:ok, _post} ->
+#        conn
+#        |> put_flash(:info, "Post Approved successfully.")
+#        |> redirect(to: ~p"/posts")
+#
+#      {:error, %Ecto.Changeset{} = changeset} ->
+#        render(conn, :edit, post: post, changeset: changeset)
+#    end
   end
 
   def delete(conn, %{"id" => id}) do
-    post = CRUD.get_post!(id)
-    {:ok, _post} = CRUD.delete_post(post)
+#    post = CRUD.get_post!(id)
+    post = get_post_by_id(id)
+    table_name = "news2post_dev_Posts_v2"
+    Dynamo.delete_item(
+      table_name,
+      %{id: post.id, created_at: post.created_at}
+    )
+    |> ExAws.request!
 
     conn
     |> put_flash(:info, "Post deleted successfully.")
     |> redirect(to: ~p"/posts")
+  end
+
+  def all_posts_v2(table_name) do
+    Dynamo.scan(
+      table_name,
+      limit: 5,
+    )
+    |> ExAws.request!
+    |> Dynamo.decode_item(as: Post)
+  end
+
+  def get_post_by_id(id) do
+    table_name = "news2post_dev_Posts_v2"
+    result = Dynamo.query(
+      table_name,
+      #      limit: 10,
+      expression_attribute_values: [id: id],
+      expression_attribute_names: %{"#id" => "id"},
+      key_condition_expression: "#id = :id"
+    )
+    |> ExAws.request!
+    |> Dynamo.decode_item(as: Post)
+
+    Enum.at(result, 0)
   end
 end
