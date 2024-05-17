daaeed26298c6e78056c8c97619293502f75035a
Thanh <tatthanh.dev@gmail.com>
2024-05-17 14:17:28 +0700

    code refactor

--- lib/date_helpers.ex
+++ lib/date_helpers.ex
@@ -4,4 +4,24 @@ defmodule News2Post.DateHelpers do
   def month_name(month) do
     Enum.at(@months, month - 1)
   end
+
+  def is_today?(item_datetime, now) do
+    item_datetime.year == now.year and
+    item_datetime.month == now.month and
+    item_datetime.day == now.day
+  end
+
+  def is_this_month?(item_datetime, now) do
+    item_datetime.year == now.year and
+    item_datetime.month == now.month
+  end
+
+  def is_this_quarter?(item_datetime, now) do
+    item_datetime.year == now.year and
+    div(item_datetime.month - 1, 3) == div(now.month - 1, 3)
+  end
+
+  def is_this_year?(item_datetime, now) do
+    item_datetime.year == now.year
+  end
 end
--- lib/news_2_post_web/controllers/dashboard_controller.ex
+++ lib/news_2_post_web/controllers/dashboard_controller.ex
@@ -95,23 +95,23 @@ defmodule News2PostWeb.DashboardController do
 
     today = Enum.count(items, fn item ->
       item_datetime = parse_datetime(item.created_at)
-      check = is_today?(item_datetime, now)
+      check = DateHelpers.is_today?(item_datetime, now)
       check
     end)
 
     this_month = Enum.count(items, fn item ->
       item_datetime = parse_datetime(item.created_at)
-      is_this_month?(item_datetime, now)
+      DateHelpers.is_this_month?(item_datetime, now)
     end)
 
     this_quarter = Enum.count(items, fn item ->
       item_datetime = parse_datetime(item.created_at)
-      is_this_quarter?(item_datetime, now)
+      DateHelpers.is_this_quarter?(item_datetime, now)
     end)
 
     this_year = Enum.count(items, fn item ->
       item_datetime = parse_datetime(item.created_at)
-      is_this_year?(item_datetime, now)
+      DateHelpers.is_this_year?(item_datetime, now)
     end)
 
     %{
@@ -130,23 +130,4 @@ defmodule News2PostWeb.DashboardController do
        end
   end
 
-  defp is_today?(item_datetime, now) do
-    item_datetime.year == now.year and
-    item_datetime.month == now.month and
-    item_datetime.day == now.day
-  end
-
-  defp is_this_month?(item_datetime, now) do
-    item_datetime.year == now.year and
-    item_datetime.month == now.month
-  end
-
-  defp is_this_quarter?(item_datetime, now) do
-    item_datetime.year == now.year and
-    div(item_datetime.month - 1, 3) == div(now.month - 1, 3)
-  end
-
-  defp is_this_year?(item_datetime, now) do
-    item_datetime.year == now.year
-  end
 end
